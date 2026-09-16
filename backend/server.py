d1f0e970896ee0876060a8be916fd1467fc11eb8
Shahriar Matin <shahriarmatin1@gmail.com>
2026-09-16 17:07:49 +0600

    Enable CORS for backend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 DATA_FILE = os.path.join(
     os.path.dirname(__file__),
     "admin_translations.json"
